1f2156752d23ec3f6080d337c691fd07bd213387
inkstatim <t1m32sw@gmail.com>
2025-04-14 15:26:10 +0200

    constants

---
+++ config/constants.py
@@ -0,0 +1,19 @@
+BASE_URL = "https://api.pomidor-stage.ru"
+
+AUTH_HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "application/json"
+}
+
+API_HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+
+AUTH_DATA = {
+    "username": "mitatskni@gmail.com",
+    "password": "qwertyuio",
+    "scope": "",
+    "client_id": "",
+    "client_secret": ""
+}
