af57f561aa184676ed3ab2f991f24ac3b9a9720e
inkstatim <t1m32sw@gmail.com>
2025-04-16 17:21:41 +0200

    add user registration fixture for each session

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,40 @@ from config.constants import BASE_URL, AUTH_HEADERS, AUTH_DATA, API_HEADERS
 fake = Faker()
 
 @pytest.fixture(scope="session")
-def auth_session():
+def register():
     session = requests.Session()
-    response = session.post(f"{BASE_URL}/api/v1/login/access-token", data=AUTH_DATA, headers=AUTH_HEADERS)
+
+    email = f"{fake.user_name()}@test.com"
+    password = fake.password(length=10)
+    user_data = {
+        "email": email,
+        "password": password,
+        "full_name": fake.name()
+    }
+
+    response = session.post(f"{BASE_URL}/api/v1/users/signup", json=user_data)
+    assert response.status_code == 200, f"Registration failed: {response.text}"
+
+    return {
+        "email": email,
+        "password": password
+    }
+
+@pytest.fixture
+def login_data(register):
+    return {
+        "username": register["email"],
+        "password": register["password"],
+        "scope": "",
+        "client_id": "",
+        "client_secret": ""
+    }
+
+
+@pytest.fixture
+def auth_session(login_data):
+    session = requests.Session()
+    response = session.post(f"{BASE_URL}/api/v1/login/access-token", data=login_data, headers=AUTH_HEADERS)
     assert response.status_code == 200
     token = response.json().get("access_token")
     assert token
@@ -16,7 +47,7 @@ def auth_session():
     session.headers.update({"Authorization": f"Bearer {token}"})
     return session
 
-@pytest.fixture()
+@pytest.fixture
 def item_data():
     return {
         "title": fake.word().capitalize(),
