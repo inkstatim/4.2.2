c03e31fa2eece6e012dc0ae1c5cd563be695f433
inkstatim <t1m32sw@gmail.com>
2025-04-16 17:22:34 +0200

    add test for user profile retrieval

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,18 @@
 from config.constants import BASE_URL, AUTH_HEADERS, AUTH_DATA
 import requests
+import allure
 
-def test_auth_token_retrieval():
-    response = requests.post(f"{BASE_URL}/api/v1/login/access-token", data=AUTH_DATA, headers=AUTH_HEADERS)
+
+@allure.feature("Authentication")
+@allure.title("Получение профиля пользователя")
+def test_get_profile(auth_session):
+    response = auth_session.get(f"{BASE_URL}/api/v1/users/me")
+    assert response.status_code == 200
+
+@allure.feature("Authentication")
+@allure.title("Получение профиля пользователя")
+def test_auth_token_retrieval(login_data):
+    response = requests.post(f"{BASE_URL}/api/v1/login/access-token", data=login_data, headers=AUTH_HEADERS)
     assert response.status_code == 200, f"Auth failed: {response.status_code}, {response.text}"
     token = response.json().get("access_token")
     assert token is not None, "Token not found"
